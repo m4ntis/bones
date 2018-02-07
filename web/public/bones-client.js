c153cc6a6b1efcf442bd976dad70a39de0067587
Mati Shabtay <matishabtay@gmail.com>
2018-02-07 19:44:42 +0200

    added basic msg handling structure

--- web/public/bones-client.js
+++ web/public/bones-client.js
@@ -1,3 +1,6 @@
+const SET_PIXEL = 0
+const SET_PALLETTE = 1
+
 function init_canvas(name) {
     var canvas = document.getElementById(name);
     var ctx = canvas.getContext("2d");
@@ -10,6 +13,25 @@ function init_canvas(name) {
 
 function init() {
     [canvas, ctx] = init_canvas("screen");
+    
+    con = new WebSocket("ws://" + document.location.host + "/ws");
+    con.onmessage = onMessage;
+}
+
+function onMessage(evt) {
+    var buffer = new Uint8Array(evt.data);
+    if buffer[0] == SET_PIXEL {
+        setPixel(buffer);
+    else {
+        setPallette(buffer);
+    }
+}
+
+// buffer = type(1), x(1), y(1), colour(1)
+function setPixel(buffer) {
+}
+
+function setPallette(buffer) {
 }
 
 init();

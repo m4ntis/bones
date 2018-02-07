283cfcfa4704aeff94c92716b7c733ca24aa7542
Mati Shabtay <matishabtay@gmail.com>
2018-02-07 16:31:11 +0200

    Using http.FileServer

--- web/main.go
+++ web/main.go
@@ -2,25 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	f, err := os.Open("index.html")
-	defer f.Close()
-	if err != nil {
-		fmt.Fprint(w, "index.html not found")
-		return
-	}
-
-	w.Header().Set("Content-Type", "text/html")
-	io.Copy(w, f)
-}
-
 func handleHTTP() {
-	http.HandleFunc("/", handler)
+	http.Handle("/", http.FileServer(http.Dir(".")))
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		fmt.Println(err)

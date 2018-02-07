ae9b3c8f066380b3f6ef1e9be805e98ccd4269a6
Mati Shabtay <matishabtay@gmail.com>
2018-02-07 21:46:56 +0200

    implemented web server

--- web/main.go
+++ web/main.go
@@ -1,15 +1,67 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/gorilla/websocket"
 )
 
 var (
-	log = logrus.WithField("cmd", "web")
+	log      = logrus.WithField("cmd", "web")
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
 )
+
+func getPallette() []byte {
+	b := []byte{1, 0, 255, 0, 0, 1, 0, 255, 0, 2, 0, 0, 255}
+	return b
+}
+
+func getColour() []byte {
+	x := byte(rand.Intn(256))
+	y := byte(rand.Intn(256))
+	i := byte(rand.Intn(3))
+
+	colour := make([]byte, 4)
+	colour[0] = 0
+	colour[1] = x
+	colour[2] = y
+	colour[3] = i
+
+	return colour
+}
+
+func handleClient(ws *websocket.Conn) {
+	ws.WriteMessage(websocket.BinaryMessage, getPallette())
+
+	for {
+		ws.WriteMessage(websocket.BinaryMessage, getColour())
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func handleWebsocket(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		m := "Unable to upgrade to websockets"
+		log.WithField("err", err).Println(m)
+		http.Error(w, m, http.StatusBadRequest)
+		return
+	}
+
+	go handleClient(ws)
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -18,5 +70,6 @@ func main() {
 	}
 
 	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/ws", handleWebsocket)
 	log.Println(http.ListenAndServe(":"+port, nil))
 }

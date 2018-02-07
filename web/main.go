134c2aa7d1788626d210a750134e08926eb860ef
Mati Shabtay <matishabtay@gmail.com>
2018-02-08 00:18:55 +0200

    client and sever basic functionality works

--- web/main.go
+++ web/main.go
@@ -19,7 +19,21 @@ var (
 )
 
 func getPallette() []byte {
-	b := []byte{1, 0, 255, 0, 0, 1, 0, 255, 0, 2, 0, 0, 255}
+	//b := []byte{1, 0, 255, 0, 0, 1, 0, 255, 0, 2, 0, 0, 255}
+	b := make([]byte, 13)
+	b[0] = 1
+	b[1] = 0
+	b[2] = 255
+	b[3] = 0
+	b[4] = 0
+	b[5] = 1
+	b[6] = 0
+	b[7] = 255
+	b[8] = 0
+	b[9] = 2
+	b[10] = 0
+	b[11] = 0
+	b[12] = 255
 	return b
 }
 
@@ -38,11 +52,12 @@ func getColour() []byte {
 }
 
 func handleClient(ws *websocket.Conn) {
+	time.Sleep(100 * time.Millisecond)
 	ws.WriteMessage(websocket.BinaryMessage, getPallette())
 
 	for {
 		ws.WriteMessage(websocket.BinaryMessage, getColour())
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(10 * time.Millisecond)
 	}
 }
 
--- web/public/bones-client.js
+++ web/public/bones-client.js
@@ -2,13 +2,15 @@ const SET_PIXEL = 0
 const SET_PALLETTE = 1
 
 var pallette = []
+var arrayBuffer = null;
+var fileReader = new FileReader();
+fileReader.onload = function() {
+    arrayBuffer = this.result;
+};
 
 function init_canvas(name) {
     var canvas = document.getElementById(name);
     var ctx = canvas.getContext("2d");
-
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0,0,768,672);
 
     return [canvas, ctx];
 }
@@ -21,7 +23,9 @@ function init() {
 }
 
 function onMessage(evt) {
-    var buffer = new Uint8Array(evt.data);
+    fileReader.readAsArrayBuffer(evt.data);
+
+    var buffer = new Uint8Array(arrayBuffer);
 
     var code = buffer[0];
     if (code == SET_PIXEL) {
@@ -44,12 +48,19 @@ function setPixel(buffer) {
 // buffer = type(1), index(1), r(1), g(1), b(1), ...
 function setPallette(buffer) {
     for(i=1; i<buffer.byteLength; i+=4) {
-        pallette[i] = hexCodeFromBytes(buffer[i+1], buffer[i+2], buffer[i+3]);
+        pallette[buffer[i]] = hexCodeFromBytes(buffer[i+1], buffer[i+2], buffer[i+3]);
     }
 }
 
 function hexCodeFromBytes(r, g, b) {
-    return "#" + r.toString(16) + g.toString(16) + b.toString(16);
+    return "#" + hexCodeFromByte(r) + hexCodeFromByte(g) + hexCodeFromByte(b)
+}
+
+function hexCodeFromByte(b) {
+    if (b < 16) {
+        return "0" + b.toString(16);
+    }
+    return b.toString(16);
 }
 
 init();

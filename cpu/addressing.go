28df7f8d31ab8959b5c68411565236c1c981e52f
Mati Shabtay <matishabtay@gmail.com>
2018-02-12 00:29:07 +0200

    Added the basic structure for opcodes and addressing modes

---
+++ cpu/addressing.go
@@ -0,0 +1,8 @@
+package cpu
+
+// AddressingMode defines one of the 2a03's ways of addressing the operands of
+// the different opcodes.
+//
+// Each addressing mode is responsible of fetching the operands in it's way,
+// and calling the operation with them
+type AddressingMode func(Operation, ...byte)
---
+++ cpu/opcode.go
@@ -0,0 +1,26 @@
+package cpu
+
+// Operation defines an operation that the CPU executes in one or more of it's
+// opcodes.
+//
+// The byte values it received are it's operands
+type Operation func(...byte)
+
+// OpCode defines an opcode of the 2a03.
+//
+// It has it's textual representation, it's addressing mode and the operation
+// itself
+type OpCode struct {
+	name string
+
+	mode AddressingMode
+	op   Operation
+}
+
+// Exec runs the opcode with the given arguments.
+//
+// It runs it's addressing mode, which in turn fetches the operands and calls
+// the operation
+func (op OpCode) Exec(args ...byte) {
+	mode(op, args)
+}

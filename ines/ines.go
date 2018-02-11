66d9b0dcc49399204d7646aa3e9d6d76c05f41bf
Mati Shabtay <matishabtay@gmail.com>
2018-02-11 15:13:54 +0200

    build succeeds now

--- ines/ines.go
+++ ines/ines.go
@@ -21,14 +21,14 @@ type inesHeader struct {
 	ChrROMSize int
 
 	Mirroring        int
-	PersistentMemory bool
-	Trainer          bool
-	IgnoreMirror     bool
+	PersistentMemory int
+	Trainer          int
+	IgnoreMirror     int
 	MapperNumber     int
 }
 
-func readHeader(r *io.Reader) (header []byte, err error) {
-	header := make([]byte, INES_HEADER_SIZE)
+func readHeader(r io.Reader) (header []byte, err error) {
+	header = make([]byte, INES_HEADER_SIZE)
 	n, err := r.Read(header)
 	if n < INES_HEADER_SIZE && err == nil {
 		return nil, errors.Errorf("Couldn't read enough bytes, read %d/%d", n,
@@ -36,7 +36,7 @@ func readHeader(r *io.Reader) (header []byte, err error) {
 	} else if err != nil {
 		return nil, errors.WithStack(err)
 	}
-	return b, nil
+	return header, nil
 }
 
 // parseHeader parses the slice it gets into an inesHeader struct.
@@ -44,45 +44,45 @@ func readHeader(r *io.Reader) (header []byte, err error) {
 // This method expects the slice to be of size 16, and panics if shorter and
 // disregards the trailing data if longer, so the caller must ensure that the
 // sent buffer is of len() >= 16.
-func parseHeader(header []byte) (header inesHeader, err error) {
-	if !bytes.Equal(header[:3], []byte{0x4e, 0x45, 0x53, 0x1a}) {
+func parseHeader(headerBuff []byte) (header *inesHeader, err error) {
+	if !bytes.Equal(headerBuff[:3], []byte{0x4e, 0x45, 0x53, 0x1a}) {
 		return nil, errors.Errorf("Incorrect iNes header prefix: %s",
-			hex.Dump(header[:3]))
+			hex.Dump(headerBuff[:3]))
 	}
 
-	prgROMSize := header[4]
+	prgROMSize := headerBuff[4]
 	if prgROMSize == 0 {
 		return nil, errors.New("PRG ROM size can't be 0")
 	}
 
-	chrROMSize := header[5]
+	chrROMSize := headerBuff[5]
 	if chrROMSize == 0 {
 		chrROMSize = 1
 	}
 
-	mirroring := header[6] & 1
-	persistentMemory := header[6] & 2
-	trainer := header[6] & 4
-	ignoreMirror := header[6] & 8
-	mapperNumber := header[6] & 240
+	mirroring := headerBuff[6] & 1
+	persistentMemory := headerBuff[6] & 2
+	trainer := headerBuff[6] & 4
+	ignoreMirror := headerBuff[6] & 8
+	mapperNumber := headerBuff[6] & 240
 
-	return inesHeader{
-		PrgROMSize: prgROMSize,
-		ChrROMSize: chrROMSize,
+	return &inesHeader{
+		PrgROMSize: int(prgROMSize),
+		ChrROMSize: int(chrROMSize),
 
-		Mirroring:        mirroring,
-		PersistentMemory: persistentMemory,
-		Trainer:          trainer,
-		IgnoreMirror:     ignoreMirror,
-		MapperNumber:     mapperNumber,
-	}
+		Mirroring:        int(mirroring),
+		PersistentMemory: int(persistentMemory),
+		Trainer:          int(trainer),
+		IgnoreMirror:     int(ignoreMirror),
+		MapperNumber:     int(mapperNumber),
+	}, nil
 }
 
 // Parse reads from the reader, parses the data and populates a ROM
 // struct accordingly.
 //
 // The errors it returns may relate to reading errors or ines format errors
-func Parse(r *io.Reader) (rom *models.ROM, err error) {
+func Parse(r io.Reader) (rom *models.ROM, err error) {
 	// Read and parse header
 	headerBuff, err := readHeader(r)
 	if err != nil {
@@ -100,26 +100,30 @@ func Parse(r *io.Reader) (rom *models.ROM, err error) {
 	romSize := trainerSize + prgROMSize + chrROMSize
 
 	romBuff := make([]byte, romSize)
-	n := copy(romBuff, r)
-	if n < romSize {
-		return nil, errors.Errorf("Not enough data in rom, %d/%d", n, romSize)
+	n, err := r.Read(romBuff)
+	if n > 0 && n < romSize {
+		return nil, errors.Errorf("Not enough data in ROM, %d/%d", n, romSize)
+	} else if err != nil {
+		return nil, errors.Wrap(err, "Error while reading ROM")
 	}
 
 	// Populate ROM fields
 	var trainer models.Trainer
 	copy(trainer[:], romBuff[:models.TRAINER_SIZE])
 
-	prgROM := make([]PrgROMPage, header.PrgROMSize)
+	prgROM := make([]models.PrgROMPage, header.PrgROMSize)
 	for i := range prgROM {
 		startIndex := trainerSize + i*models.PRG_ROM_PAGE_SIZE
-		copy(prgROM[i][:], r[startIndex:startIndex+models.PRG_ROM_PAGE_SIZE])
+		copy(prgROM[i][:],
+			romBuff[startIndex:startIndex+models.PRG_ROM_PAGE_SIZE])
 	}
 
-	chrROM := make([]ChrROMPage, header.ChrROMSize)
+	chrROM := make([]models.ChrROMPage, header.ChrROMSize)
 	for i := range chrROM {
 		startIndex := trainerSize + prgROMSize + i*models.CHR_ROM_PAGE_SIZE
-		copy(chrROM[i][:], r[startIndex:startIndex+models.CHR_ROM_PAGE_SIZE])
+		copy(chrROM[i][:],
+			romBuff[startIndex:startIndex+models.CHR_ROM_PAGE_SIZE])
 	}
 
-	return NewRom(trainer, prgROM, chrROM), nil
+	return models.NewROM(trainer, prgROM, chrROM), nil
 }

78067e963236da98e925d000ead344161e312cc9
Mati Shabtay <matishabtay@gmail.com>
2018-02-12 10:30:51 +0200

    documentation

--- cpu/opcode.go
+++ cpu/opcode.go
@@ -3,10 +3,10 @@ package cpu
 // Operation defines an operation that the CPU executes in one or more of it's
 // opcodes.
 //
-// The byte values it received are it's operands. Both operands and return val
-// might be nil, depends on the operation. There isn't a gurantee that the
-// operation will check for the correct number of operands, so make sure you
-// pass in the correct amount.
+// The byte values it received are it's arguments. Both arguments and return
+// value might be nil, depends on the operation. There isn't a gurantee that
+// the operation will check for the correct number of arguments, so make sure
+// you pass in the correct amount.
 type Operation func(...byte) interface{}
 
 // OpCode defines an opcode of the 2a03.
@@ -24,7 +24,7 @@ type OpCode struct {
 
 // Exec runs the opcode with the given arguments.
 //
-// It runs it's addressing mode, which in turn fetches the operands and calls
+// It runs it's addressing mode, which in turn fetches the arguments and calls
 // the operation
 func (op OpCode) Exec(args []byte) {
 	op.mode(op.oper, args)

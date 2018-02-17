127e8e0d6317120c2a68585627aa7bc55a4578a1
Mati Shabtay <matishabtay@gmail.com>
2018-02-17 20:26:10 +0200

    implemented cpu stack

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -5,3 +5,17 @@ type CPU struct {
 
 	reg *Registers
 }
+
+func (cpu *CPU) push(b byte) {
+	cpu.ram.Write(getStackAddr())
+	cpu.reg.sp--
+}
+
+func (cpu *CPU) pull() byte {
+	cpu.reg.sp++
+	return cpu.ram.Read(getStackAddr())
+}
+
+func (cpu *CPU) getStackAddr() int {
+	return int(cpu.reg.sp) | (1 << 8)
+}

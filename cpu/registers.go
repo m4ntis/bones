a7b1210277050fff214611c829ea3055229faaf2
Mati Shabtay <matishabtay@gmail.com>
2018-02-19 22:49:21 +0200

    minor build fix

--- cpu/registers.go
+++ cpu/registers.go
@@ -21,3 +21,18 @@ type Registers struct {
 	v byte
 	n byte
 }
+
+func (reg *Registers) getP() byte {
+	return reg.c | reg.z<<1 | reg.i<<2 | reg.d<<3 | reg.b<<4 | reg.v<<6 |
+		reg.n<<7
+}
+
+func (reg *Registers) setP(p byte) {
+	reg.c = p & 1
+	reg.z = p & 2
+	reg.i = p & 4
+	reg.d = p & 8
+	reg.b = p & 16
+	reg.v = p & 64
+	reg.n = p & 128
+}

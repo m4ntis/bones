8a3372e884b7521d88789bd24130ba86676dc078
Mati Shabtay <matishabtay@gmail.com>
2018-03-19 03:18:50 +0200

    Implement basic ppu bg rendering

--- ppu/ppu.go
+++ ppu/ppu.go
@@ -1,6 +1,10 @@
 package ppu
 
-import "github.com/m4ntis/bones/models"
+import (
+	"image/color"
+
+	"github.com/m4ntis/bones/models"
+)
 
 type PPU struct {
 	RAM *RAM
@@ -38,4 +42,41 @@ func (ppu *PPU) LoadROM(rom *models.ROM) {
 
 func (ppu *PPU) DMA(oamData [256]byte) {
 	ppu.OAM = &OAM{oamData}
+}
+
+func (ppu *PPU) Cycle(scanline int, x int) color.RGBA {
+	if scanline >= 0 && scanline < 240 {
+		pt := int(*ppu.Reg.PPUCTRL >> 4 & 1)
+		nt := (scanline/8)*32 + x/8
+		at := (scanline/32)*8 + x/32
+
+		// For now we assume nametable 0
+		ntByte := *ppu.RAM.Fetch(NT0_IDX + nt)
+
+		patternAddr := 0x1000*pt + int(ntByte)*16
+
+		ptx := x % 8
+		pty := scanline % 8
+
+		ptLowByte := *ppu.RAM.Fetch(patternAddr + pty)
+		ptLowBit := ptLowByte >> uint(ptx) & 1
+		ptHighByte := *ppu.RAM.Fetch(patternAddr + pty + 8)
+		ptHighBit := ptHighByte >> uint(ptx) & 1
+
+		peAddrLow := ptLowBit + ptHighBit<<1
+
+		atQuarter := x%32/16 + scanline%32/16<<1
+
+		// Assuming nametable 0, as mentioned above
+		atByte := *ppu.RAM.Fetch(AT0_IDX + at)
+
+		peAddrHigh := atByte >> uint(2*atQuarter) & 3
+
+		peAddr := peAddrLow + peAddrHigh<<2
+
+		pIdx := *ppu.RAM.Fetch(BGR_PALETTE_IDX + int(peAddr))
+
+		return Palette[pIdx]
+	}
+	return color.RGBA{}
 }

87aa2baef938eb4f5c5b6b00f2490b67356a41ba
Mati Shabtay <matishabtay@gmail.com>
2018-03-22 00:53:03 +0200

    Implement PPU Mapping on ram

--- cpu/ram.go
+++ cpu/ram.go
@@ -1,5 +1,7 @@
 package cpu
 
+import "github.com/m4ntis/bones/ppu"
+
 const (
 	ZeroPageBeginIdx         = 0x0
 	StackBeginIdx            = 0x100
@@ -15,8 +17,22 @@ const (
 	RamSize                  = 0x10000
 )
 
+const (
+	PPUCtrl   = 0x2000
+	PPUMask   = 0x2001
+	PPUStatus = 0x2002
+	OAMAddr   = 0x2003
+	OAMData   = 0x2004
+	PPUScroll = 0x2005
+	PPUAddr   = 0x2006
+	PPUData   = 0x2007
+	OAMDMA    = 0x4014
+)
+
 type RAM struct {
 	data [RamSize]byte
+
+	ppu *ppu.PPU
 }
 
 // getAddr returns the underlying address after mapping.
@@ -35,9 +51,55 @@ func getAddr(addr int) int {
 }
 
 func (r *RAM) Read(addr int) byte {
-	return r.data[getAddr(addr)]
+	addr = getAddr(addr)
+
+	switch addr {
+	case PPUCtrl:
+		panic("Invalid read from PPUCtrl")
+	case PPUMask:
+		panic("Invalid read from PPUMask")
+	case PPUStatus:
+		return r.ppu.PPUStatusRead()
+	case OAMAddr:
+		panic("Invalid read from OAMAddr")
+	case OAMData:
+		return r.ppu.OAMDataRead()
+	case PPUScroll:
+		panic("Invalid read from PPUScroll")
+	case PPUAddr:
+		panic("Invalid read from PPUAddr")
+	case PPUData:
+		return r.ppu.PPUDataRead()
+	case OAMDMA:
+		panic("Invalid read from OAMDMA")
+	default:
+		return r.data[addr]
+	}
 }
 
 func (r *RAM) Write(addr int, d byte) {
-	r.data[getAddr(addr)] = d
+	addr = getAddr(addr)
+
+	switch addr {
+	case PPUCtrl:
+		r.ppu.PPUCtrlWrite(d)
+	case PPUMask:
+		r.ppu.PPUMaskWrite(d)
+	case PPUStatus:
+		panic("Invalid write to PPUStatus")
+	case OAMAddr:
+		r.ppu.OAMAddrWrite(d)
+	case OAMData:
+		r.ppu.OAMDataWrite(d)
+	case PPUScroll:
+		r.ppu.PPUScrollWrite(d)
+	case PPUAddr:
+		r.ppu.PPUAddrWrite(d)
+	case PPUData:
+		r.ppu.PPUDataWrite(d)
+	case OAMDMA:
+		r.ppu.OAMDMAWrite(d)
+	default:
+		r.data[addr] = d
+	}
 }

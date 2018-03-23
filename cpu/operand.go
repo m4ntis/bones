c40440d0ebd3a73bb4b8e1db5276c2b7a7ed36df
Mati Shabtay <matishabtay@gmail.com>
2018-03-23 20:09:43 +0300

    Change operand to interface

--- cpu/operand.go
+++ cpu/operand.go
@@ -1,67 +1,43 @@
 package cpu
 
-import "fmt"
-
-const (
-	ARegisterOperand = iota
-)
-
-const (
-	CPURAMOperand = iota
-	CPURegisterOperand
-	ConstOperand
-)
-
-type OperandType int
-
-type Operand struct {
-	cpu *CPU
-
-	Type  OperandType
-	Ident int
+type Operand interface {
+	Read() byte
+	Write(byte)
 }
 
-func NewOperand(cpu *CPU, ot OperandType, identifier int) Operand {
-	return Operand{
-		cpu: cpu,
-
-		Type:  ot,
-		Ident: identifier,
-	}
+type RAMOperand struct {
+	RAM  *RAM
+	Addr int
 }
 
-func (op Operand) Read() byte {
-	switch op.Type {
-	case CPURAMOperand:
-		return op.cpu.RAM.Read(op.Ident)
-	case CPURegisterOperand:
-		if op.Ident == ARegisterOperand {
-			return cpu.Regs.A
-		} else {
-			panic(fmt.Sprintf("Invalid cpu register identifier %d",
-				op.Ident))
-		}
-	case ConstOperand:
-		return byte(op.Ident)
-	default:
-		panic(fmt.Sprintf("Invalid operand type %d", op.ot))
-	}
+func (op RAMOperand) Read() byte {
+	return op.ram.Read(op.addr)
 }
 
-func (op Operand) Write(d byte) {
-	switch op.Type {
-	case CPURAMOperand:
-		return op.cpu.RAM.Write(op.Ident, d)
-	case CPURegisterOperand:
-		if op.Ident == a {
-			cpu.Regs.A = d
-		} else {
-			panic(fmt.Sprintf("Invalid cpu register identifier %d",
-				op.Ident))
-		}
-	case ConstOperand:
-		panic("Can't write to a const operand")
-	default:
-		panic(fmt.Sprintf("Invalid operand type %d", op.ot))
-	}
+func (op RAMOperand) Write(d byte) {
+	op.ram.Write(d)
+}
+
+type RegOperand struct {
+	Reg *byte
+}
+
+func (op RegOperand) Read() byte {
+	return *op.reg
+}
+
+func (op RegOperand) Write(d byte) {
+	*op.reg = d
+}
+
+type ConstOperand struct {
+	D byte
+}
+
+func (op ConstOperand) Read() byte {
+	return op.d
+}
+
+func (op ConstOperand) Write(d byte) {
+	panic("Can't write to a const operand")
 }

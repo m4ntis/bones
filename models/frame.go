99b79810c700fde86fdc1970b098ab3e817c40ae
Mati Shabtay <matishabtay@gmail.com>
2018-03-27 04:21:50 +0300

    Reset frame after generation

--- models/frame.go
+++ models/frame.go
@@ -13,9 +13,12 @@ func (f *Frame) Push(pix Pixel) {
 }
 
 func (f *Frame) Create() image.Image {
+	defer func() { f.pixels = make([]Pixel, 256*240) }()
+
 	frame := image.NewNRGBA(image.Rect(0, 0, 256, 240))
 	for _, pix := range f.pixels {
 		frame.Set(pix.X, pix.Y, pix.Color)
 	}
+
 	return frame
 }

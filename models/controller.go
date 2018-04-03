958108e556e2996f830e086a7c477c4da3a0f600
Mati Shabtay <matishabtay@gmail.com>
2018-04-03 17:00:28 +0300

    Add controller

---
+++ models/controller.go
@@ -0,0 +1,98 @@
+package models
+
+const (
+	ButtonPressed  = 0
+	ButtonReleased = 0
+)
+
+type Controller struct {
+	aState int
+	bState int
+
+	selectState int
+	startState  int
+
+	upState    int
+	downState  int
+	leftState  int
+	rightState int
+
+	readCount int
+}
+
+func (c *Controller) Poll() {
+	c.readCount = 0
+}
+
+func (c *Controller) Read() int {
+	if c.readCount == 0 {
+		return c.aState
+	} else if c.readCount == 1 {
+		return c.bState
+	} else if c.readCount == 2 {
+		return c.selectState
+	} else if c.readCount == 3 {
+		return c.startState
+	} else if c.readCount == 4 {
+		return c.upState
+	} else if c.readCount == 5 {
+		return c.downState
+	} else if c.readCount == 6 {
+		return c.leftState
+	} else if c.readCount == 7 {
+		return c.rightState
+	} else {
+		return 1
+	}
+}
+
+func (c *Controller) PressA() {
+	c.aState = ButtonPressed
+}
+func (c *Controller) ReleaseA() {
+	c.aState = ButtonReleased
+}
+func (c *Controller) PressB() {
+	c.bState = ButtonPressed
+}
+func (c *Controller) ReleaseB() {
+	c.bState = ButtonReleased
+}
+
+func (c *Controller) PressSelect() {
+	c.selectState = ButtonPressed
+}
+func (c *Controller) ReleaseSelect() {
+	c.selectState = ButtonReleased
+}
+func (c *Controller) PressStart() {
+	c.startState = ButtonPressed
+}
+func (c *Controller) ReleaseStart() {
+	c.startState = ButtonReleased
+}
+
+func (c *Controller) PressUp() {
+	c.upState = ButtonPressed
+}
+func (c *Controller) ReleaseUp() {
+	c.upState = ButtonReleased
+}
+func (c *Controller) PressDown() {
+	c.downState = ButtonPressed
+}
+func (c *Controller) ReleaseDown() {
+	c.downState = ButtonReleased
+}
+func (c *Controller) PressLeft() {
+	c.leftState = ButtonPressed
+}
+func (c *Controller) ReleaseLeft() {
+	c.leftState = ButtonReleased
+}
+func (c *Controller) PressRight() {
+	c.rightState = ButtonPressed
+}
+func (c *Controller) ReleaseRight() {
+	c.rightState = ButtonReleased
+}

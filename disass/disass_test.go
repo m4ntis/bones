c7587b8277f087ae4ccc9480e8c29e5ab96b9ed1
Mati Shabtay <matishabtay@gmail.com>
2018-04-22 02:36:23 +0300

    Fix debug example to build

--- disass/disass_test.go
+++ disass/disass_test.go
@@ -8,14 +8,19 @@ import (
 )
 
 func Example() {
-	f, _ := os.Open("path_to_rom.nes")
-	rom, _ := ines.Parse(f)
+	f, err := os.Open("path_to_rom.nes")
+	if err != nil {
+		return
+	}
+	rom, err := ines.Parse(f)
+	if err != nil {
+		return
+	}
 
 	d := Disassemble(rom.PrgROM)
 	for _, inst := range d.Code {
 		fmt.Printf("%04x: %s\n", inst.Addr, inst.Text)
 	}
-	// Output:
 	// 8000: SEI
 	// 8001: CLD
 	// 8002: LDA #$10

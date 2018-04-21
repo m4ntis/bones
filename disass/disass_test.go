f892797f97cf87f8190bca463ac4615711d8fcf4
Mati Shabtay <matishabtay@gmail.com>
2018-04-22 02:17:02 +0300

    Documentation: Fix disass example

--- disass/disass_test.go
+++ disass/disass_test.go
@@ -2,9 +2,15 @@ package disass
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/m4ntis/bones/ines"
 )
 
 func Example() {
+	f, _ := os.Open("path_to_rom.nes")
+	rom, _ := ines.Parse(f)
+
 	d := Disassemble(rom.PrgROM)
 	for _, inst := range d.Code {
 		fmt.Printf("%04x: %s\n", inst.Addr, inst.Text)

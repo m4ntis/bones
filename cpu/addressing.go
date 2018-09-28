01c16106c940876e8839ca83f453adec5e600b49
Mati Shabtay <matishabtay@gmail.com>
2018-09-29 02:40:55 +0300

    addressing.go: Test and return an error on RAM read

--- cpu/addressing.go
+++ cpu/addressing.go
@@ -1,6 +1,23 @@
 package cpu
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+// TODO: Consider optimal method of handling PC register overflow as a result of
+// bad NES code / faulty ROM parsing.
+//
+// For example, AbsoluteX/Y addressing like 0xFFFF + X/Y (where X/Y > 0), would
+// result in an error raised in RAM accessing, ONLY when and IF the operation
+// calls Read / Write of the operand.
+//
+// This holds both for RAM Operand instanciation and for RAM reading of an
+// address (IE indirect $ffff, adh would be in $0000).
+//
+// Optimally BoNES should panic if the user is using `bones run` on the ROM, but
+// perhaps just return an error msg when doing something like debugging.
 
 // AddressingMode defines one of the mos 6502's ways of addressing operands.
 //
@@ -16,7 +33,7 @@ type AddressingMode struct {
 	OpsLen int
 	Format func([]byte) string
 
-	Address func(*CPU, Operation, bool, ...byte) int
+	Address func(*CPU, Operation, bool, ...byte) (int, error)
 }
 
 var (
@@ -25,10 +42,11 @@ var (
 		OpsLen: 0,
 		Format: func(ops []byte) string { return "" },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			cpu.Reg.PC++
 			op(cpu, NilOperand{})
-			return
+			return extraCycles, nil
 		},
 	}
 
@@ -37,10 +55,11 @@ var (
 		OpsLen: 0,
 		Format: func(ops []byte) string { return "A" },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			op(cpu, RegOperand{Reg: &cpu.Reg.A})
 			cpu.Reg.PC++
-			return
+			return extraCycles, nil
 		},
 	}
 
@@ -49,10 +68,11 @@ var (
 		OpsLen: 1,
 		Format: func(ops []byte) string { return fmt.Sprintf("#$%02x", ops[0]) },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			op(cpu, ConstOperand{D: ops[0]})
 			cpu.Reg.PC += 2
-			return
+			return extraCycles, nil
 		},
 	}
 
@@ -61,10 +81,11 @@ var (
 		OpsLen: 1,
 		Format: func(ops []byte) string { return fmt.Sprintf("$%02x", ops[0]) },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			op(cpu, RAMOperand{RAM: cpu.RAM, Addr: int(ops[0])})
 			cpu.Reg.PC += 2
-			return
+			return extraCycles, nil
 		},
 	}
 
@@ -73,10 +94,11 @@ var (
 		OpsLen: 1,
 		Format: func(ops []byte) string { return fmt.Sprintf("$%02x, X", ops[0]) },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			op(cpu, RAMOperand{RAM: cpu.RAM, Addr: int(ops[0] + cpu.Reg.X)})
 			cpu.Reg.PC += 2
-			return
+			return extraCycles, nil
 		},
 	}
 
@@ -85,10 +107,11 @@ var (
 		OpsLen: 1,
 		Format: func(ops []byte) string { return fmt.Sprintf("$%02x, Y", ops[0]) },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			op(cpu, RAMOperand{RAM: cpu.RAM, Addr: int(ops[0] + cpu.Reg.Y)})
 			cpu.Reg.PC += 2
-			return
+			return extraCycles, nil
 		},
 	}
 
@@ -97,10 +120,11 @@ var (
 		OpsLen: 1,
 		Format: func(ops []byte) string { return fmt.Sprintf("$%02x", ops[0]) },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			extraCycles = op(cpu, ConstOperand{D: ops[0]})
 			cpu.Reg.PC += 2
-			return
+			return extraCycles, nil
 		},
 	}
 
@@ -109,14 +133,15 @@ var (
 		OpsLen: 2,
 		Format: func(ops []byte) string { return fmt.Sprintf("$%02x%02x", ops[1], ops[0]) },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			// We inc this beforehand so that JSR wont be incremented after
 			// execution
 			cpu.Reg.PC += 3
 
 			addr := int(ops[0]) | int(ops[1])<<8
 			op(cpu, RAMOperand{RAM: cpu.RAM, Addr: addr})
-			return
+			return extraCycles, nil
 		},
 	}
 
@@ -125,7 +150,8 @@ var (
 		OpsLen: 2,
 		Format: func(ops []byte) string { return fmt.Sprintf("$%02x%02x, X", ops[1], ops[0]) },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			addr := int(ops[0]) | int(ops[1])<<8
 			xAddr := addr + int(cpu.Reg.X)
 
@@ -135,7 +161,7 @@ var (
 
 			op(cpu, RAMOperand{RAM: cpu.RAM, Addr: xAddr})
 			cpu.Reg.PC += 3
-			return
+			return extraCycles, nil
 		},
 	}
 
@@ -144,7 +170,8 @@ var (
 		OpsLen: 2,
 		Format: func(ops []byte) string { return fmt.Sprintf("$%02x%02x, Y", ops[1], ops[0]) },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			addr := int(ops[0]) | int(ops[1])<<8
 			yAddr := addr + int(cpu.Reg.Y)
 
@@ -154,7 +181,7 @@ var (
 
 			op(cpu, RAMOperand{RAM: cpu.RAM, Addr: yAddr})
 			cpu.Reg.PC += 3
-			return
+			return extraCycles, nil
 		},
 	}
 
@@ -163,14 +190,22 @@ var (
 		OpsLen: 2,
 		Format: func(ops []byte) string { return fmt.Sprintf("($%02x%02x)", ops[1], ops[0]) },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			addr := int(ops[0]) + int(ops[1])<<8
 
-			adl := cpu.RAM.Read(addr)
-			adh := cpu.RAM.Read(addr + 1)
+			adl, err := cpu.RAM.Read(addr)
+			if err != nil {
+				return 0, errors.Wrap(err, "Couldn't read indirected address")
+			}
+
+			adh, err := cpu.RAM.Read(addr + 1)
+			if err != nil {
+				return 0, errors.Wrap(err, "Couldn't read indirected address")
+			}
 
 			op(cpu, RAMOperand{RAM: cpu.RAM, Addr: int(adl) | int(adh)<<8})
-			return
+			return extraCycles, nil
 		},
 	}
 
@@ -179,15 +214,23 @@ var (
 		OpsLen: 1,
 		Format: func(ops []byte) string { return fmt.Sprintf("($%02x, X)", ops[0]) },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			addr := int(ops[0] + cpu.Reg.X)
 
-			adl := cpu.RAM.Read(addr)
-			adh := cpu.RAM.Read((addr + 1) % 0x100)
+			adl, err := cpu.RAM.Read(addr)
+			if err != nil {
+				return 0, errors.Wrap(err, "Couldn't read indirected address")
+			}
+
+			adh, err := cpu.RAM.Read((addr + 1) % 0x100)
+			if err != nil {
+				return 0, errors.Wrap(err, "Couldn't read indirected address")
+			}
 
 			op(cpu, RAMOperand{RAM: cpu.RAM, Addr: int(adl) | int(adh)<<8})
 			cpu.Reg.PC += 2
-			return
+			return extraCycles, nil
 		},
 	}
 
@@ -196,11 +239,19 @@ var (
 		OpsLen: 1,
 		Format: func(ops []byte) string { return fmt.Sprintf("($%02x), Y", ops[0]) },
 
-		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool, ops ...byte) (extraCycles int) {
+		Address: func(cpu *CPU, op Operation, pageBoundryCheck bool,
+			ops ...byte) (extraCycles int, err error) {
 			addr := int(ops[0])
 
-			adl := cpu.RAM.Read(addr)
-			adh := cpu.RAM.Read((addr + 1) % 0x100)
+			adl, err := cpu.RAM.Read(addr)
+			if err != nil {
+				return 0, errors.Wrap(err, "Couldn't read indirected address")
+			}
+
+			adh, err := cpu.RAM.Read((addr + 1) % 0x100)
+			if err != nil {
+				return 0, errors.Wrap(err, "Couldn't read indirected address")
+			}
 
 			fetched := int(adl) | int(adh)<<8
 			if fetched/256 != (fetched+int(cpu.Reg.Y))/256 && pageBoundryCheck {
@@ -210,7 +261,7 @@ var (
 
 			op(cpu, RAMOperand{RAM: cpu.RAM, Addr: fetched})
 			cpu.Reg.PC += 2
-			return
+			return extraCycles, nil
 		},
 	}
 )

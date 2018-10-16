a1df156de5765c0145f560d57b5b30cc5313514d
Mati Shabtay <matishabtay@gmail.com>
2018-10-16 23:00:14 +0300

    ppu.go: Giant algorithm cleanup

--- ppu/ppu.go
+++ ppu/ppu.go
@@ -40,37 +40,23 @@ type Displayer interface {
 // they are interfaces via memory mapped i/o on the CPU RAM.
 type PPU struct {
 	VRAM *VRAM
+	OAM  *OAM
 
-	OAM     *OAM
-	sOAM    *secondaryOAM
+	Regs *Registers
+
+	NMI chan bool
+
 	sprites [8]sprite
+	sOAM    *secondaryOAM
 
-	evalSprN      int
-	foundSprCount int
+	evaluatedSprNum   int
+	foundSprCount     int
+	spriteZeroPresent bool
 
 	scanline int
 	x        int
 
-	ppuCtrl   byte
-	ppuMask   byte
-	ppuStatus byte
-
-	oamAddr byte
-
-	scrollFirstWrite bool
-	xScroll          int
-	yScroll          int
-
-	addrFirstWrite bool
-	ppuAddr        int
-
 	mirror int
-
-	ppuData    byte
-	ppuDataBuf byte
-
-	vblank bool
-	nmi    chan bool
 
 	frame *frame
 	disp  Displayer
@@ -78,154 +64,34 @@ type PPU struct {
 
 // New initializes a PPU instance and returns it.
 //
-// nmi is the channel on which the PPU publishes NMIs.
+// NMI is the channel on which the PPU publishes NMIs.
 //
 // disp is the where the PPU outputs its frames.
-func New(mirror int, romMapper mapper.Mapper, nmi chan bool, disp Displayer) *PPU {
-	vram := VRAM{
+func New(mirror int, romMapper mapper.Mapper, disp Displayer) *PPU {
+	vram := &VRAM{
 		Mapper: romMapper,
 	}
 
-	var oam OAM
-	var soam secondaryOAM
+	oam := &OAM{}
+	soam := &secondaryOAM{}
+
+	nmi := make(chan bool)
 
 	return &PPU{
-		VRAM: &vram,
+		VRAM: vram,
+		Regs: newRegisters(nmi, oam, vram),
 
-		OAM:     &oam,
-		sOAM:    &soam,
+		OAM:     oam,
+		sOAM:    soam,
 		sprites: [8]sprite{},
-
-		scrollFirstWrite: true,
-		addrFirstWrite:   true,
 
 		mirror: mirror,
 
-		vblank: false,
-		nmi:    nmi,
+		NMI: nmi,
 
 		frame: newFrame(),
 		disp:  disp,
 	}
-}
-
-// Cycle executes a single PPU cycle.
-//
-// Cycle may cause the ppu to generate an NMI or output a frame to the display.
-func (ppu *PPU) Cycle() {
-	if ppu.scanline >= 0 && ppu.scanline < 240 {
-		ppu.spriteEval()
-
-		if ppu.x < 256 {
-			ppu.frame.push(pixel{
-				x: ppu.x,
-				y: ppu.scanline,
-
-				color: ppu.visiblePixelCycle(),
-			})
-		}
-	} else if ppu.scanline == 241 && ppu.x == 1 {
-		ppu.vblank = true
-		ppu.ppuStatus |= 1 << 7
-		if ppu.ppuCtrl>>7 == 1 {
-			ppu.nmi <- true
-		}
-
-		ppu.disp.Display(ppu.frame.create())
-	} else if ppu.scanline == 261 && ppu.x == 1 {
-		// Pre-render line dot 1, clear ppu status and end vblank
-		ppu.ppuStatus = 0
-		ppu.vblank = false
-	}
-
-	ppu.incCoords()
-}
-
-func (ppu *PPU) PPUCtrlWrite(data byte) {
-	// If V Flag set (and changed) while in vblank
-	if ppu.ppuCtrl>>7 == 0 && data>>7 == 1 && ppu.vblank {
-		ppu.nmi <- true
-	}
-
-	ppu.ppuCtrl = data
-}
-
-func (ppu *PPU) PPUMaskWrite(data byte) {
-	ppu.ppuMask = data
-}
-
-func (ppu *PPU) PPUStatusRead() byte {
-	defer func() {
-		// Clear bit 7
-		ppu.ppuStatus &= 0x7f
-
-		ppu.scrollFirstWrite = true
-		ppu.xScroll = 0
-		ppu.yScroll = 0
-
-		ppu.ppuAddr = 0
-	}()
-
-	return ppu.ppuStatus
-}
-
-func (ppu *PPU) OAMAddrWrite(data byte) {
-	ppu.oamAddr = data
-}
-
-func (ppu *PPU) OAMDataRead() byte {
-	return (*ppu.OAM)[ppu.oamAddr]
-}
-
-func (ppu *PPU) OAMDataWrite(data byte) {
-	ppu.oamAddr++
-
-	(*ppu.OAM)[ppu.oamAddr] = data
-}
-
-// TODO: Changes made to the vertical scroll during rendering will only take
-// effect on the next frame
-
-func (ppu *PPU) PPUScrollWrite(data byte) {
-	defer func() { ppu.scrollFirstWrite = !ppu.scrollFirstWrite }()
-
-	if ppu.scrollFirstWrite {
-		ppu.xScroll = int(data)
-		return
-	}
-
-	ppu.yScroll = int(data)
-}
-
-func (ppu *PPU) PPUAddrWrite(data byte) {
-	defer func() { ppu.addrFirstWrite = !ppu.addrFirstWrite }()
-
-	if ppu.addrFirstWrite {
-		ppu.ppuAddr = int(data) << 8
-		return
-	}
-	ppu.ppuAddr |= int(data)
-}
-
-func (ppu *PPU) PPUDataRead() byte {
-	defer ppu.incAddr()
-
-	// If the read is from palette data, it is immediatelly put on the data bus
-	if stripMirror(ppu.ppuAddr) >= BgrPaletteAddr {
-		// TODO: Reading the palettes still updates the internal buffer though,
-		// but the data placed in it is the mirrored nametable data that would
-		// appear "underneath" the palette.
-		return ppu.VRAM.Read(ppu.ppuAddr)
-	}
-
-	defer func() { ppu.ppuDataBuf = ppu.VRAM.Read(ppu.ppuAddr) }()
-	return ppu.ppuDataBuf
-}
-
-func (ppu *PPU) PPUDataWrite(d byte) {
-	defer ppu.incAddr()
-
-	ppu.VRAM.Write(ppu.ppuAddr, d)
 }
 
 //TODO: Take note of oamaddr
@@ -236,8 +102,141 @@ func (ppu *PPU) DMA(oamData [256]byte) {
 	ppu.OAM = &oam
 }
 
-func (ppu *PPU) incAddr() {
-	ppu.ppuAddr += int(1 + (ppu.ppuCtrl>>2&1)*31)
+// Cycle executes a single PPU cycle.
+//
+// Cycle may cause the ppu to generate an NMI or output a frame to the display.
+func (ppu *PPU) Cycle() {
+	if ppu.scanline >= 0 && ppu.scanline < 240 {
+		ppu.visibleScanlineCycle()
+	} else if ppu.scanline == 241 && ppu.x == 1 {
+		ppu.vblankBegin()
+	} else if ppu.scanline == 261 && ppu.x == 1 {
+		ppu.vblankEnd()
+	}
+
+	ppu.incCoords()
+}
+
+func (ppu *PPU) visibleScanlineCycle() {
+	ppu.evaluateSprites()
+
+	if ppu.x < 256 {
+		ppu.frame.push(pixel{
+			x: ppu.x,
+			y: ppu.scanline,
+
+			color: ppu.visiblePixelCycle(),
+		})
+	}
+}
+
+func (ppu *PPU) vblankBegin() {
+	ppu.Regs.vblank = true
+	ppu.Regs.ppuStatus |= 1 << 7
+	if ppu.Regs.ppuCtrl>>7 == 1 {
+		ppu.NMI <- true
+	}
+
+	ppu.disp.Display(ppu.frame.create())
+}
+
+func (ppu *PPU) vblankEnd() {
+	ppu.Regs.ppuStatus = 0
+	ppu.Regs.vblank = false
+}
+
+func (ppu *PPU) evaluateSprites() {
+	if ppu.x >= 1 && ppu.x <= 256 {
+		if ppu.x == 1 {
+			ppu.resetEvaluatedSprites()
+		}
+
+		if ppu.evaluatedSprNum < 64 {
+			ppu.evaluateSprite()
+			ppu.evaluatedSprNum++
+		}
+
+		ppu.shiftSprites()
+	} else if ppu.x >= 257 && ppu.x <= 320 {
+		// Once every 8 cycles
+		if ppu.x%8 == 1 {
+			ppu.renderSprite()
+		}
+	}
+}
+
+func (ppu *PPU) resetEvaluatedSprites() {
+	ppu.evaluatedSprNum = 0
+	ppu.foundSprCount = 0
+	ppu.spriteZeroPresent = false
+	ppu.sOAM = &secondaryOAM{}
+}
+
+// evaluateSprite checks if the evaluated
+func (ppu *PPU) evaluateSprite() {
+	sprData := ppu.OAM[ppu.evaluatedSprNum*sprDataSize : (ppu.evaluatedSprNum+1)*sprDataSize]
+
+	// Check sprite in range of next scanline
+	if ppu.scanline >= int(sprData[0]) && ppu.scanline < int(sprData[0])+8 {
+		if ppu.foundSprCount >= 8 {
+			// Set overflow flag
+			ppu.Regs.ppuStatus &= 1 << 5
+		} else {
+			// Copy sprite data from OAM to secondary OAM
+			copy(ppu.sOAM[ppu.foundSprCount*sprDataSize:(ppu.foundSprCount+1)*sprDataSize],
+				sprData)
+			ppu.spriteZeroPresent = true
+		}
+
+		ppu.foundSprCount++
+	}
+}
+
+// renderSprite fetches the colours and data of a sprite to be displayed on
+// the next frame
+func (ppu *PPU) renderSprite() {
+	// Determine which sprite is being rendered
+	renderedSprNum := (ppu.x - 256) / 8
+
+	// Check whether the sprite is in range of the displayed sprites. If not,
+	// all it's data will be 0xff.
+	if ppu.foundSprCount >= renderedSprNum+1 {
+		// Determine pattern table
+		pt := int(ppu.Regs.ppuCtrl >> 3 & 1)
+		patternAddr := 0x1000*pt + int(ppu.sOAM[renderedSprNum*4+1])*16
+
+		// Line of the sprite to be displayed on the scanline
+		sprLine := ppu.scanline - int(ppu.sOAM[renderedSprNum*4])
+
+		// TODO: Might gotta invert this
+		sprDataLo := ppu.VRAM.Read(patternAddr + sprLine)
+		sprDataHi := ppu.VRAM.Read(patternAddr + sprLine + 8)
+
+		attr := ppu.sOAM[renderedSprNum*4+2]
+		// Check horizontal invert bit off
+		if attr>>6&1 == 0 {
+			sprDataLo = flip_byte(sprDataLo)
+			sprDataHi = flip_byte(sprDataHi)
+		}
+
+		ppu.sprites[renderedSprNum] = sprite{
+			attr: ppu.sOAM[renderedSprNum*4+2],
+
+			dataHi: sprDataHi,
+			dataLo: sprDataLo,
+
+			x: ppu.sOAM[renderedSprNum*4+3],
+		}
+	} else {
+		ppu.sprites[renderedSprNum] = sprite{
+			attr: 0xff,
+
+			dataHi: 0xff,
+			dataLo: 0xff,
+
+			x: 0xff,
+		}
+	}
 }
 
 // TODO: skip cycle (0, 0) on odd frames
@@ -254,14 +253,14 @@ func (ppu *PPU) incCoords() {
 }
 
 func (ppu *PPU) visiblePixelCycle() color.RGBA {
-	scrolledX := ppu.x + ppu.xScroll
+	scrolledX := ppu.x + ppu.Regs.xScroll
 
 	// Select Pattern Table, Name Table and Attribute Table
 	pt := ppu.getPTAddr()
 	nt := (ppu.scanline/8)*32 + scrolledX%256/8
 	at := (ppu.scanline/32)*8 + scrolledX%256/32
 
-	ntBase := getNTAddr(int(ppu.ppuCtrl)&3+scrolledX/256, ppu.mirror)
+	ntBase := getNTAddr(int(ppu.Regs.ppuCtrl)&3+scrolledX/256, ppu.mirror)
 	ntByte := ppu.VRAM.Read(ntBase + nt)
 
 	patternAddr := pt + int(ntByte)*16
@@ -282,14 +281,19 @@ func (ppu *PPU) visiblePixelCycle() color.RGBA {
 
 	bgHi := atByte >> uint(2*atQuarter) & 3
 
-	sprLo, sprHi, sprPriority := ppu.calcSprForPixel()
+	sprLo, sprHi, sprPriority, sprN := ppu.calcSprForPixel()
 	pAddr := ppu.calcPaletteAddr(bgLo, bgHi, sprLo, sprHi, sprPriority)
+
+	if ppu.spriteZeroPresent && sprN == 0 && int(sprLo+sprHi<<2) != 0 && int(bgLo+bgHi<<2) != 0 {
+		// Set sprite 0 hit flag
+		ppu.Regs.ppuStatus |= 1 << 6
+	}
 
 	return Palette[pAddr]
 }
 
 func (ppu *PPU) getPTAddr() int {
-	if int(ppu.ppuCtrl>>4&1) == 0 {
+	if int(ppu.Regs.ppuCtrl>>4&1) == 0 {
 		return PT0Addr
 	} else {
 		return PT1Addr
@@ -325,78 +329,6 @@ func (ppu *PPU) calcPaletteAddr(bgLo, bgHi, sprLo, sprHi, sprPriority byte) (pAd
 	}
 
 	return ppu.VRAM.Read(BgrPaletteAddr + int(bgLo) + int(bgHi<<2))
-}
-
-func (ppu *PPU) spriteEval() {
-	if ppu.x >= 1 && ppu.x <= 256 {
-		if ppu.x == 1 {
-			// Reset ppu sprite eval flags
-			ppu.evalSprN = 0
-			ppu.foundSprCount = 0
-			ppu.sOAM = &secondaryOAM{}
-		}
-		if ppu.evalSprN < 64 {
-			// Sprite in scanline range
-			if ppu.scanline >= int(ppu.OAM[ppu.evalSprN*4]) &&
-				ppu.scanline < int(ppu.OAM[ppu.evalSprN*4])+8 {
-				if ppu.foundSprCount >= 8 {
-					// Set overflow flag
-					ppu.ppuStatus &= 1 << 5
-				} else {
-					// Copy 4 bytes of sprite data from OAM to secondary OAM
-					copy(ppu.sOAM[ppu.foundSprCount*4:ppu.foundSprCount*4+4], ppu.OAM[ppu.evalSprN*4:ppu.evalSprN*4+4])
-					ppu.foundSprCount++
-				}
-			}
-
-			ppu.evalSprN++
-		}
-
-		ppu.shiftSprites()
-	} else if ppu.x >= 257 && ppu.x <= 320 {
-		// Gotta do this once every 8 cycles
-		if ppu.x%8 == 1 {
-			sprN := (ppu.x - 256) / 8
-
-			if ppu.foundSprCount >= sprN+1 {
-				// Determine pattern table
-				pt := int(ppu.ppuCtrl >> 3 & 1)
-				patternAddr := 0x1000*pt + int(ppu.sOAM[sprN*4+1])*16
-
-				// Line of the sprite to be displayed on the scanline
-				sprLine := ppu.scanline - int(ppu.sOAM[sprN*4])
-
-				// TODO: Might gotta invert this
-				sprDataLo := ppu.VRAM.Read(patternAddr + sprLine)
-				sprDataHi := ppu.VRAM.Read(patternAddr + sprLine + 8)
-
-				attr := ppu.sOAM[sprN*4+2]
-				// Check horizontal invert bit off
-				if attr>>6&1 == 0 {
-					sprDataLo = flip_byte(sprDataLo)
-					sprDataHi = flip_byte(sprDataHi)
-				}
-
-				ppu.sprites[sprN] = sprite{
-					attr: ppu.sOAM[sprN*4+2],
-
-					dataHi: sprDataHi,
-					dataLo: sprDataLo,
-
-					x: ppu.sOAM[sprN*4+3],
-				}
-			} else {
-				ppu.sprites[sprN] = sprite{
-					attr: 0xff,
-
-					dataHi: 0xff,
-					dataLo: 0xff,
-
-					x: 0xff,
-				}
-			}
-		}
-	}
 }
 
 // shiftSprites is implemented in a duff machine fashion for optimization
@@ -478,56 +410,56 @@ func (ppu *PPU) shiftSprites() {
 
 // calcSprForPixel is implemented in a duff machine fashion for optimization
 // purposes.
-func (ppu *PPU) calcSprForPixel() (sprLo, sprHi, priority byte) {
+func (ppu *PPU) calcSprForPixel() (sprLo, sprHi, priority byte, sprN int) {
 	if ppu.sprites[0].x == 0 && ppu.sprites[0].shifted < 8 {
 		sprLo := ppu.sprites[0].dataLo&1 + ppu.sprites[0].dataHi&1<<1
 		if sprLo != 0 {
-			return sprLo, ppu.sprites[0].attr & 3, ppu.sprites[0].attr >> 5 & 1
+			return sprLo, ppu.sprites[0].attr & 3, ppu.sprites[0].attr >> 5 & 1, 0
 		}
 	}
 	if ppu.sprites[1].x == 0 && ppu.sprites[1].shifted < 8 {
 		sprLo := ppu.sprites[1].dataLo&1 + ppu.sprites[1].dataHi&1<<1
 		if sprLo != 0 {
-			return sprLo, ppu.sprites[1].attr & 3, ppu.sprites[1].attr >> 5 & 1
+			return sprLo, ppu.sprites[1].attr & 3, ppu.sprites[1].attr >> 5 & 1, 1
 		}
 	}
 	if ppu.sprites[2].x == 0 && ppu.sprites[2].shifted < 8 {
 		sprLo := ppu.sprites[2].dataLo&1 + ppu.sprites[2].dataHi&1<<1
 		if sprLo != 0 {
-			return sprLo, ppu.sprites[2].attr & 3, ppu.sprites[2].attr >> 5 & 1
+			return sprLo, ppu.sprites[2].attr & 3, ppu.sprites[2].attr >> 5 & 1, 2
 		}
 	}
 	if ppu.sprites[3].x == 0 && ppu.sprites[3].shifted < 8 {
 		sprLo := ppu.sprites[3].dataLo&1 + ppu.sprites[3].dataHi&1<<1
 		if sprLo != 0 {
-			return sprLo, ppu.sprites[3].attr & 3, ppu.sprites[3].attr >> 5 & 1
+			return sprLo, ppu.sprites[3].attr & 3, ppu.sprites[3].attr >> 5 & 1, 3
 		}
 	}
 	if ppu.sprites[4].x == 0 && ppu.sprites[4].shifted < 8 {
 		sprLo := ppu.sprites[4].dataLo&1 + ppu.sprites[4].dataHi&1<<1
 		if sprLo != 0 {
-			return sprLo, ppu.sprites[4].attr & 3, ppu.sprites[4].attr >> 5 & 1
+			return sprLo, ppu.sprites[4].attr & 3, ppu.sprites[4].attr >> 5 & 1, 4
 		}
 	}
 	if ppu.sprites[5].x == 0 && ppu.sprites[5].shifted < 8 {
 		sprLo := ppu.sprites[5].dataLo&1 + ppu.sprites[5].dataHi&1<<1
 		if sprLo != 0 {
-			return sprLo, ppu.sprites[5].attr & 3, ppu.sprites[5].attr >> 5 & 1
+			return sprLo, ppu.sprites[5].attr & 3, ppu.sprites[5].attr >> 5 & 1, 5
 		}
 	}
 	if ppu.sprites[6].x == 0 && ppu.sprites[6].shifted < 8 {
 		sprLo := ppu.sprites[6].dataLo&1 + ppu.sprites[6].dataHi&1<<1
 		if sprLo != 0 {
-			return sprLo, ppu.sprites[6].attr & 3, ppu.sprites[6].attr >> 5 & 1
+			return sprLo, ppu.sprites[6].attr & 3, ppu.sprites[6].attr >> 5 & 1, 6
 		}
 	}
 	if ppu.sprites[7].x == 0 && ppu.sprites[7].shifted < 8 {
 		sprLo := ppu.sprites[7].dataLo&1 + ppu.sprites[7].dataHi&1<<1
 		if sprLo != 0 {
-			return sprLo, ppu.sprites[7].attr & 3, ppu.sprites[7].attr >> 5 & 1
+			return sprLo, ppu.sprites[7].attr & 3, ppu.sprites[7].attr >> 5 & 1, 7
 		}
 	}
-	return 0, 0, 255
+	return 0, 0, 255, -1
 }
 
 func flip_byte(d byte) byte {

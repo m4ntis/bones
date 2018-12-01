82fce55bd759e5bb517b100d4691b1fa2ccd4f8b
Mati Shabtay <matishabtay@gmail.com>
2018-12-01 02:09:55 +0200

    dbg.go: Merge with dbg_commands.go

--- bones/cmd/dbg.go
+++ bones/cmd/dbg.go
@@ -3,24 +3,21 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/m4ntis/bones"
+	"github.com/m4ntis/bones/cpu"
 	"github.com/m4ntis/bones/ines"
 	"github.com/m4ntis/bones/io"
+	"github.com/m4ntis/bones/ppu"
 	"github.com/peterh/liner"
 	"github.com/spf13/cobra"
 )
 
 var (
-	n *bones.NES
-
-	dbgCommands map[string]*dbgCommand
-	help        string
-
-	line      = liner.NewLiner()
-	lastInput = ""
-
 	// dbgCmd represents the dbg cli command
 	dbgCmd = &cobra.Command{
 		Use:   "dbg",
@@ -120,20 +117,20 @@ func handleUserInput(data bones.BreakState) (finished bool) {
 
 	args := strings.Fields(input)
 
-	cmd, ok := dbgCommands[args[0]]
+	cmd, ok := cmdsMap[args[0]]
 	if !ok {
 		fmt.Printf("%s isn't a valid command, type 'help' for a list\n", input)
 		return false
 	}
 
-	return cmd.cmd(data, args[1:])
+	return cmd.run(data, args[1:])
 }
 
 func init() {
 	rootCmd.AddCommand(dbgCmd)
 
-	dbgCommands = createCommands()
-	help = generateHelp()
+	cmdsMap = mapCmds(cmds)
+	help = generateHelp(cmds)
 
 	// Make bones dbg's usage be 'bones dbg <romname>.nes'
 	dbgCmd.SetUsageTemplate(`Usage:
@@ -160,3 +157,347 @@ Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
 Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
 `)
 }
+
+// dbgCommand represents a command of the interactive debugger
+//
+// The return value of the function is set if the user interaction has ended,
+// and the debugger should return to waiting for the next breakpoint to be hit.
+type dbgCommand struct {
+	name    string
+	aliases []string
+
+	cmd  func(data bones.BreakState, args []string) bool
+	argc int
+
+	description string
+	usage       string
+	hString     string
+}
+
+func (d *dbgCommand) run(data bones.BreakState, args []string) (ok bool) {
+	ok = d.validateArgsLength(args)
+	if !ok {
+		return false
+	}
+
+	return d.cmd(data, args)
+}
+
+func (d *dbgCommand) generateCmdHelpTitle() string {
+	if len(d.aliases) == 0 {
+		return d.name
+	}
+
+	return fmt.Sprintf("%s (alias: %s)", d.name, strings.Join(d.aliases, " | "))
+}
+
+func (d *dbgCommand) validateArgsLength(args []string) (ok bool) {
+	if d.argc == 0 {
+		return true
+	}
+
+	if len(args) != d.argc {
+		if d.argc == 1 {
+			fmt.Printf("%s command takes exactly 1 argument\n", d.name)
+			return false
+		}
+
+		fmt.Printf("%s command takes exactly %d arguments\n", d.name, d.argc)
+		return false
+	}
+
+	return true
+}
+
+// mapCmds maps commands to their title.
+func mapCmds(cmds []dbgCommand) map[string]*dbgCommand {
+	cmdsMap := map[string]*dbgCommand{}
+
+	for i := range cmds {
+		cmdsMap[cmds[i].name] = &cmds[i]
+
+		for _, a := range cmds[i].aliases {
+			cmdsMap[a] = &cmds[i]
+		}
+	}
+
+	return cmdsMap
+}
+
+func printHelp(args []string) {
+	if len(args) == 0 {
+		fmt.Println(help)
+		return
+	}
+
+	if len(args) > 1 {
+		fmt.Println("help command only takes up to 1 arguments")
+		return
+	}
+
+	cmd, ok := cmdsMap[args[0]]
+	if !ok {
+		fmt.Printf("%s isn't a valid command, type 'help' for a list\n", args[0])
+		return
+	}
+
+	fmt.Println(cmd.description)
+	if cmd.usage != "" {
+		fmt.Println()
+		fmt.Println("    " + cmd.usage)
+	}
+	if cmd.hString != "" {
+		fmt.Println()
+		fmt.Println(cmd.hString)
+	}
+}
+
+// generateHelp generates an alphabetically sorted, multi-line help string for a
+// list of commands, based on their name, aliases and description.
+func generateHelp(cmds []dbgCommand) string {
+	// descriptions maps a cmd's title to it's description
+	descriptions := map[string]string{}
+
+	// titles holds a sorted list of titles
+	titles := make([]string, 0)
+
+	// maxTitleLen holds the length of longest title
+	maxTitleLen := 0
+
+	// Map cmd title to description
+	for _, cmd := range cmds {
+		title := cmd.generateCmdHelpTitle()
+		if len(title) > maxTitleLen {
+			maxTitleLen = len(title)
+		}
+
+		descriptions[title] = cmd.description
+	}
+
+	// Sort titles
+	for title := range descriptions {
+		titles = append(titles, title)
+	}
+	sort.Strings(titles)
+
+	help := "The following commands are available:\n"
+	for _, title := range titles {
+		help += fmt.Sprintf("    %s %s %s\n",
+			title,
+			strings.Repeat("-", maxTitleLen-len(title)+1),
+			descriptions[title])
+	}
+
+	help += "Type 'help' followed by a command for full documentation"
+	return help
+}
+
+var (
+	n *bones.NES
+
+	cmdsMap map[string]*dbgCommand
+	help    string
+
+	line      = liner.NewLiner()
+	lastInput = ""
+
+	cmds = []dbgCommand{
+		dbgCommand{
+			name:    "break",
+			aliases: []string{"b"},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				addr, err := strconv.ParseInt(args[0], 16, 32)
+				if err != nil {
+					fmt.Println("break command only takes a numeric value in base 16")
+					return false
+				}
+
+				n.Break(int(addr))
+				fmt.Printf("Breakpoint set at $%04x\n", addr)
+
+				return false
+			},
+			argc: 1,
+
+			description: "Set a breakpoint",
+			usage:       "break <address>",
+			hString:     "Sets a breakpoint at the specified address in base 16",
+		},
+		dbgCommand{
+			name:    "breakpoints",
+			aliases: []string{"bps"},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				for _, addr := range n.List() {
+					fmt.Printf("%04x\n", addr)
+				}
+				return false
+			},
+
+			description: "List breakpoints",
+		},
+		dbgCommand{
+			name:    "delete",
+			aliases: []string{"del", "d"},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				addr, err := strconv.ParseInt(args[0], 16, 32)
+				if err != nil {
+					fmt.Println("delete command only takes a numeric value in base 16")
+					return false
+				}
+
+				ok := n.Delete(int(addr))
+				if !ok {
+					fmt.Printf("There is no breakpoint set at $%04x\n", addr)
+					return false
+				}
+
+				fmt.Printf("Deleted breakpoint at $%04x\n", addr)
+				return false
+			},
+			argc: 1,
+
+			description: "Delete a breakpoint",
+			usage:       "delete <address>",
+			hString:     "Delete a set breakpoint at the specified address in base 16",
+		},
+		dbgCommand{
+			name:    "deleteall",
+			aliases: []string{"da"},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				n.DeleteAll()
+				return false
+			},
+
+			description: "Delete all breakpoints",
+		},
+		dbgCommand{
+			name:    "continue",
+			aliases: []string{"c"},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				n.Continue()
+				return true
+			},
+
+			description: "Continue running the CPU until next break or error",
+		},
+		dbgCommand{
+			name:    "next",
+			aliases: []string{"n"},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				n.Next()
+				return true
+			},
+
+			description: "Step over to next opcode",
+		},
+		dbgCommand{
+			name:    "exit",
+			aliases: []string{"quit", "q"},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				os.Exit(0)
+				return true
+			},
+
+			description: "Exit the debugger",
+		},
+		dbgCommand{
+			name:    "help",
+			aliases: []string{"h", "?"},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				printHelp(args)
+				return false
+			},
+
+			description: "Get list of commands or help on each",
+			usage:       "help [command]",
+			hString:     "Type 'help' to get a list of commands, or help about a specific command by appending it's name",
+		},
+		dbgCommand{
+			name:    "clear",
+			aliases: []string{},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				// TODO: support windows :(
+				cmd := exec.Command("clear")
+				cmd.Stdout = os.Stdout
+				cmd.Run()
+				return false
+			},
+
+			description: "Clear the screen",
+		},
+		dbgCommand{
+			name:    "print",
+			aliases: []string{"p"},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				addr, err := strconv.ParseInt(args[0], 16, 32)
+				if err != nil || addr < 0 || addr >= cpu.RAMSize {
+					fmt.Printf("print command only takes a numeric value between 0 and 0x%x\n", cpu.RAMSize)
+					return false
+				}
+
+				// An error check is guranteed to be unnecessary as we checked
+				// bounds earlier
+				d, _ := data.RAM.Observe(int(addr))
+				fmt.Printf("$%04x: 0x%02x\n", int(addr), d)
+				return false
+			},
+			argc: 1,
+
+			description: "Print a value from RAM",
+			usage:       "print <address>",
+			hString:     "Prints the hex value from RAM at a given address in hex",
+		},
+		dbgCommand{
+			name:    "vprint",
+			aliases: []string{"vp"},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				addr, err := strconv.ParseInt(args[0], 16, 32)
+				if err != nil || addr > ppu.RAMSize {
+					fmt.Printf("vprint command only takes a numeric value between 0 and 0x%x\n", cpu.RAMSize)
+					return false
+				}
+
+				fmt.Printf("$%04x: 0x%02x\n", int(addr), data.VRAM.Read(int(addr)))
+				return false
+			},
+			argc: 1,
+
+			description: "Print a value from VRAM",
+			usage:       "vprint <address>",
+			hString:     "Prints the hex value from VRAM at a given address in hex",
+		},
+		dbgCommand{
+			name:    "regs",
+			aliases: []string{},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				fmt.Println(strings.Trim(fmt.Sprintf("%+v", data.Reg), "&{}"))
+				return false
+			},
+
+			description: "Prints the cpu's registers' status",
+		},
+		dbgCommand{
+			name:    "list",
+			aliases: []string{"ls"},
+
+			cmd: func(data bones.BreakState, args []string) bool {
+				displayBreak(data)
+				return false
+			},
+
+			description: "Display source code and current location",
+		},
+	}
+)

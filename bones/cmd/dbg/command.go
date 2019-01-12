f631135bc0ae33bff7894e27ca6b164d274be64f
Mati Shabtay <matishabtay@gmail.com>
2019-01-12 14:50:45 +0200

    command.go: Remove BreakState and fin from command signature

--- bones/cmd/dbg/command.go
+++ bones/cmd/dbg/command.go
@@ -3,8 +3,6 @@ package dbg
 import (
 	"fmt"
 	"strings"
-
-	"github.com/m4ntis/bones"
 )
 
 // Command represents a command of the interactive debugger.
@@ -12,7 +10,7 @@ type Command struct {
 	name  string
 	alias []string
 
-	cmd       func(b bones.BreakState, args []string) (fin bool)
+	cmd       func(args []string)
 	validArgs func(args []string) (ok bool)
 
 	descr string
@@ -23,15 +21,15 @@ type Command struct {
 // Run runs the command.
 //
 // fin indicates whether user interraction has finished.
-func (c *Command) Run(b bones.BreakState, args []string) (fin bool) {
+func (c *Command) Run(args []string) {
 	if c.validArgs != nil {
 		ok := c.validArgs(args)
 		if !ok {
-			return false
+			return
 		}
 	}
 
-	return c.cmd(b, args)
+	c.cmd(args)
 }
 
 func (c *Command) title() string {

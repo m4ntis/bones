3af0ff13be5a1059b2c9e20c0ca082214a2d8843
Mati Shabtay <matishabtay@gmail.com>
2019-01-12 14:49:07 +0200

    list.go: BreakState -> Break

--- bones/cmd/dbg/list.go
+++ bones/cmd/dbg/list.go
@@ -6,7 +6,7 @@ import (
 	"github.com/m4ntis/bones"
 )
 
-func List(b bones.BreakState) {
+func List(b bones.Break) {
 	for i, inst := range b.Code {
 		if i == b.PCIdx {
 			fmt.Printf("=> %04x: %s\n", inst.Addr, inst.Text)
